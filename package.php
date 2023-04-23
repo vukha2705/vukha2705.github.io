9da7872625b4847883dd83b498a6445ab1dc32e0
vukha2705 <128904787+vukha2705@users.noreply.github.com>
2023-04-23 23:06:55 +0700

    Add files via upload

---
+++ package.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>about </title>
+	<meta charset= "utf-8"/>
+	<meta name ="description" content = "DLLT From"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- swiper css link -->
+	<link
+  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+	 
+
+	<!-- font awesome cdn link -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+	<!-- link to css --> 
+	<link rel="stylesheet" href="css/tests.css">
+
+
+</head>
+
+<body>
+	
+<!-- header section starts -->
+<section class="header">
+
+
+	<a href="home.php" class = "logo">travel.</a>
+
+	<nav class="navbar">
+		<a href="home.php">Home</a>
+		<a href="about.php">About</a>
+		<a href="package.php">Package</a>
+		<a href="book.php">Book</a>
+	</nav>
+
+	<div id="menu-btn" class="fas fa-bars"></div>
+
+
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading"style="background:url(https://wallpaper.dog/large/5550890.jpg) no-repeat">
+	<h1>packages</h1>
+</div>
+
+
+
+</section>
+
+
+
+<!-- services section ends -->
+
+<!-- packages section starts -->
+
+<section class="packages">
+	 
+	 <h1 class="heading-title">top destinations</h1>
+
+	 <div class="box_container">
+	 		
+	 		<div class="box">
+	 			<div class="image">
+					 <img src="assets/pak-img1.png" alt="">				
+	 			</div>
+	 			<div class="content">
+	 				<h3>adventure & tour</h3>
+	 				<p>Travel is the only thing you buy that makes you richer.</p>
+	 				<a href="book.php" class="btn">book now</a>
+	 			</div>
+	 		</div>
+
+	 		<div class="box">
+	 			<div class="image">
+					 <img src="assets/pak-img2.png" alt="">				
+	 			</div>
+	 			<div class="content">
+	 				<h3>adventure & tour</h3>
+	 				<p>Travel is the only thing you buy that makes you richer.</p>
+	 				<a href="book.php" class="btn">book now</a>
+	 			</div>
+	 		</div>
+
+	 		<div class="box">
+	 			<div class="image">
+					 <img src="assets/pak-img3.png" alt="">				
+	 			</div>
+	 			<div class="content">
+	 				<h3>adventure & tour</h3>
+	 				<p>Travel is the only thing you buy that makes you richer.</p>
+	 				<a href="book.php" class="btn">book now</a>
+	 			</div>
+	 		</div>
+
+	 		<div class="box">
+	 			<div class="image">
+					 <img src="assets/pak-img4.png" alt="">				
+	 			</div>
+	 			<div class="content">
+	 				<h3>adventure & tour</h3>
+	 				<p>Travel is the only thing you buy that makes you richer.</p>
+	 				<a href="book.php" class="btn">book now</a>
+	 			</div>
+	 		</div>
+
+	 		<div class="box">
+	 			<div class="image">
+					 <img src="assets/pak-img5.png" alt="">				
+	 			</div>
+	 			<div class="content">
+	 				<h3>adventure & tour</h3>
+	 				<p>Travel is the only thing you buy that makes you richer.</p>
+	 				<a href="book.php" class="btn">book now</a>
+	 			</div>
+	 		</div>
+
+	 		<div class="box">
+	 			<div class="image">
+					 <img src="assets/pak-img6.png" alt="">				
+	 			</div>
+	 			<div class="content">
+	 				<h3>adventure & tour</h3>
+	 				<p>Travel is the only thing you buy that makes you richer.</p>
+	 				<a href="book.php" class="btn">book now</a>
+	 			</div>
+	 		</div>
+
+	 		<div class="box">
+	 			<div class="image">
+					 <img src="assets/pak-img7.png" alt="">
+				</div>
+	 			<div class="content">
+	 				<h3>adventure & tour</h3>
+	 				<p>Travel is the only thing you buy that makes you richer.</p>
+	 				<a href="book.php" class="btn">book now</a>
+	 			</div>
+	 		</div>
+
+	 		<div class="box">
+	 			<div class="image">
+					 <img src="assets/pak-img8.png" alt="">				
+	 			</div>
+	 			<div class="content">
+	 				<h3>adventure & tour</h3>
+	 				<p>Travel is the only thing you buy that makes you richer.</p>
+	 				<a href="book.php" class="btn">book now</a>
+	 			</div>
+	 		</div>
+
+	 		<div class="box">
+	 			<div class="image">
+					 <img src="assets/pak-img19.png" alt="">				
+	 			</div>
+	 			<div class="content">
+	 				<h3>adventure & tour</h3>
+	 				<p>Travel is the only thing you buy that makes you richer.</p>
+	 				<a href="book.php" class="btn">book now</a>
+	 			</div>
+	 		</div>
+
+	 		<div class="box">
+	 			<div class="image">
+					 <img src="assets/pak-img10.png" alt="">				
+	 			</div>
+	 			<div class="content">
+	 				<h3>adventure & tour</h3>
+	 				<p>Travel is the only thing you buy that makes you richer.</p>
+	 				<a href="book.php" class="btn">book now</a>
+	 			</div>
+	 		</div>
+
+	 </div>
+
+	 <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+<!-- footer section -->
+<section class="footer">
+
+	<div class="box_container">
+		<div class="box">
+			<h3>quick links</h3>
+			<a href="home.php"><i = class ="fas fa-angle-right"></i> Home</a>
+			<a href="about.php"><i = class ="fas fa-angle-right"></i>  About</a>
+			<a href="package.php"><i = class ="fas fa-angle-right"></i>  Package</a>
+			<a href="book.php"><i = class ="fas fa-angle-right"></i>  Book</a>
+			</div>
+		
+		<div class="box">
+			<h3>extra links</h3>
+			<a href="#"><i = class ="fas fa-angle-right"></i> ask questions</a>
+			<a href="#"><i = class ="fas fa-angle-right"></i> about us</a>
+			<a href="#"><i = class ="fas fa-angle-right"></i> privacy policy</a>
+			<a href="#"><i = class ="fas fa-angle-right"></i> terms of use</a>
+		</div>
+
+		<div class="box">
+			<h3>contact information</h3>
+			<a href="#"><i = class ="fas fa-phone"></i>+093470169</a>
+			<a href="#"><i = class ="fas fa-envelope"></i>anhkai321@gmail.com</a>
+			<a href="#"><i = class ="fas fa-map"></i>Ho Chi Minh , Viet Nam</a>
+		</div>
+		<div class="box">
+			<h3>Follow us</h3>
+			<a href="#"> <i class="fab fa-facebook"></i>Facebook</a>
+			<a href="#"> <i class="fab fa-instagram"></i>Intasgram</a>
+
+		</div>
+	</div>
+
+	<div class="credit">created by <span>vukha2705</span>| Try do it myseft!</div>
+
+
+</section>	
+
+
+
+<!-- swiper js link  -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+
+
+<script src="js/tests.js"></script>
+
+
+</body>
+</html>
